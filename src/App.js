354a31a2be31ab35ebee06cbd89114d35693d036
EttLo <ettlo.business@gmail.com>
2022-07-26 16:06:20 +0200

    inzio frontend seconda funzione

--- src/App.js
+++ src/App.js
@@ -7,17 +7,23 @@ import PlayerDetail from './PlayerDetail';
 function App() {
   const [player, setPlayer] = useState([]);
 
-  const getPlayersByTeamName = () => {
-    axios.get("http://localhost:8080/api/player?teamName=INTER")
+  const getPlayersByTeamName = (teamName) => {
+    let url = "http://localhost:8080/api/player?teamName=" + { teamName }
+    axios.get()
       .then(res => setPlayer(res.data));
   };
+  const getPlayersBye = () => {
+    axios.get("http://localhost:8080/api/player?nGoals=1")
+      .then(res => setPlayer(res.data));
+  };
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={getPlayersByTeamName}>Get player data</button>
+        <button onClick={getPlayersByTeamName("MILAN")}>Get player data</button>
         {
-          player.map(p => { /* editionDetail(e)*/
+          player.map(p => {
             return (
               <PlayerDetail key={p.id} player={p} />
             );
