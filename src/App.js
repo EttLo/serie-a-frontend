2290dda7941c333260ca0503a8737c80fe86fc66
EttLo <ettlo.business@gmail.com>
2022-07-26 18:45:03 +0200

    better looking ui

--- src/App.js
+++ src/App.js
@@ -3,32 +3,17 @@ import './App.css';
 import axios from 'axios';
 import { useState } from 'react';
 import PlayerDetail from './PlayerDetail';
+import ButtonFindByTeamName from './ButtonFindByTeamName';
+import ButtonFindPlayerByMoreGoalsThan from './ButtonFindPlayerMoreGoalsThan';
 
 function App() {
-  const [player, setPlayer] = useState([]);
-
-  const getPlayersByTeamName = (teamName) => {
-    let url = "http://localhost:8080/api/player?teamName=" + { teamName }
-    axios.get()
-      .then(res => setPlayer(res.data));
-  };
-  const getPlayersBye = () => {
-    axios.get("http://localhost:8080/api/player?nGoals=1")
-      .then(res => setPlayer(res.data));
-  };
-
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={getPlayersByTeamName("MILAN")}>Get player data</button>
-        {
-          player.map(p => {
-            return (
-              <PlayerDetail key={p.id} player={p} />
-            );
-          })
-        }
+        <ButtonFindByTeamName teamName="MILAN" />
+        <ButtonFindByTeamName teamName="INTER" />
+        <ButtonFindPlayerByMoreGoalsThan nGoals="1" />
       </header>
     </div>
   );
